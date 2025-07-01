b876af7158b42549de4882fb310faae26adbd730
Gumraze-git <galaxydh4110@gmail.com>
2025-07-01 10:26:13 +0900

    feat(repository): ProductRepository 인터페이스 추가
    - Product 도큐먼트에 대한 MongoDB 저장소 인터페이스 생성.
    - 가격이 특정 값 이상인 제품을 조회하는 findByPriceGreaterThan 메서드 추가.

---
+++ src/main/java/com/example/demo_mongo_db/respository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo_mongo_db.respository;
+
+import com.example.demo_mongo_db.document.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+    List<Product> findByPriceGreaterThan(int price);
+}
